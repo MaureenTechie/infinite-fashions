bd164974c9dc29ce2062a6e00770caa103f04ab1
Maureen Nyanamba <maureenmoraa006@gmail.com>
2026-07-08 13:08:18 +0300

    added display of the butterfly background to the app page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import ButterflyBackground from "@/components/ui/ButterflyBackground";
+
 export default function Home(){
   return(
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold">
-        Welcome to Infinite Fashions
-      </h1>
+    <main className="relative min-h-screen bg-white dark:bg-slate-950 transition-colors">
+      <ButterflyBackground/>
+
+      <section className="flex min-h-screen items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-7xl font-bold">
+            Infinite Fashions
+          </h1>
+
+          <p className="mt-6 text-xl text-gray-600 dark:text-gray-300">
+            Style Beyond Imagination
+          </p>
+        </div>
+      </section>
     </main>
   );
 }
